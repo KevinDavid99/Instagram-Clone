35f56cf7a50360c9cf2f51d8cf367f482f6c5841
KevinDavid99 <kemjikaobodo@gmail.com>
2023-11-02 11:44:30 -0700

    removed a url

--- instaclone/urls.py
+++ instaclone/urls.py
@@ -28,24 +28,3 @@ urlpatterns = [
 
 
 
-
-
-
-
-# It appears that you are trying to display the likes_count from the response in your React component, but you are not getting the desired result. Here's a possible issue and a suggestion on how to fix it:
-
-# Issue:
-# In your code, you are mapping through likeCount and trying to access the likes_count as if it's directly under the likeCount array. However, in your response, the likes_count is nested inside the liked_post object.
-
-# Suggestion:
-# You need to update your code to access likes_count from the liked_post object within the likeCount array. Here's how you can modify your code to achieve that:
-
-# jsx
-# Copy code
-# {likeCount.map((likey) => (
-#   <div className={s.postcard} key={post.id}>
-#     {/* ... other code ... */}
-#     <div className={s.likes}>{likey.liked_post.likes_count}</div>
-#     {/* ... other code ... */}
-#   </div>
-# ))}
