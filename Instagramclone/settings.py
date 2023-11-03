8a38e453c3bc60e3d192c23c5afb4adda94cd195
KevinDavid99 <kemjikaobodo@gmail.com>
2023-11-03 09:27:53 -0700

    allowed host

--- Instagramclone/settings.py
+++ Instagramclone/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-x(65#7r%-nqw+pz&cu@_=^h!p7cdq@ej&cvgi_977i$8hqvh2p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False 
 
-ALLOWED_HOSTS = ["instagram-clone-api-etqy.onrender.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
