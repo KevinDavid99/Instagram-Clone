85eb02c1763c99246d1862e0b40aa4ebba0dfb65
Kemjika David <kemjika@Kemjikas-MacBook-Pro.local>
2026-06-28 09:58:51 +0100

    set debug to false for production

--- Instagramclone/settings.py
+++ Instagramclone/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x(65#7r%-nqw+pz&cu@_=^h!p7cdq@ej&cvgi_977i$8hqvh2p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
 ALLOWED_HOSTS = ["*"]
 
